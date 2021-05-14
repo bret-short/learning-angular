30f35170291afafb3c3f46eb964a892b91f4ff68
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2021-05-14 14:38:17 -0600

    learning angular

---
+++ my-angular-app/src/app/first/first.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css'],
+})
+export class FirstComponent implements OnInit {
+  title: string = 'This is my title';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
